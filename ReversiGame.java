833b95a19349ca0565e616c7764bdd1c1c3ffaf8
starpickerr <xyrking9@gmail.com>
2025-04-16 16:30:56 +0800

    Delete ReversiGame.java

--- ReversiGame.java
+++
@@ -1,9 +0,0 @@
-
-public class ReversiGame {
-    public static void main(String[] args) {
-        Game game = new Game();
-        game.initialize();
-        game.play();
-        game.end();
-    }
-}
